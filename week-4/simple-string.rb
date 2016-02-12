610ed3caf0a4f025bb0605c96bc9fb512ececd67
Macbook Air <macbookair@Macbooks-Air.netgear.com>
2016-02-12 13:33:50 -0800

    add new files

--- week-4/simple-string.rb
+++ week-4/simple-string.rb
@@ -1,7 +1,7 @@
 # Solution Below
 
 old_string = "Ruby is cool"
-new_string = "LOOC SI YBUR"
+new_string = "LOsOC SI YBUR"
 
 
 # RSpec Tests. They are included in this file because the local variables you are creating are not accessible across files. If we try to run these files as a separate file per normal operation, the local variable checks will return nil.
