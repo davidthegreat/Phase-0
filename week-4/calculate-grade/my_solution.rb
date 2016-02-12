d774979c7ab0c0e30c94a97997498cd67f0daa76
Macbook Air <macbookair@Macbooks-Air.netgear.com>
2016-02-12 11:03:57 -0800

    Add files

---
+++ week-4/calculate-grade/my_solution.rb
@@ -0,0 +1,24 @@
+# Calculate a Grade
+
+# I worked on this challenge [by myself, with: Chunyu Ou].
+
+
+# Your Solution Below
+def get_grade(num)
+
+if num >= 90
+	"A"
+elsif num >=80
+	"B"
+elsif num >= 70
+	"C"
+elsif num >= 60
+	"D"
+else 
+	"F"
+end
+
+
+
+
+end
