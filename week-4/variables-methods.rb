dc348c1276dc6f56cbd0dbc5a9ad1575d0e209e3
Macbook Air <macbookair@Macbooks-Air.netgear.com>
2016-03-02 14:31:10 -0800

    add relfetion

--- week-4/variables-methods.rb
+++ week-4/variables-methods.rb
@@ -19,4 +19,21 @@ favorite_num2 = favorite_num + 1
 
 print "You favorite number #{favorite_num} is a great number but #{favorite_num2} is a bigger and better number. "
 
-
+#RELEASE
+=begin
+How do you define a local variable?
+	A local variable is a variable located inside a method inside parameter of the method.
+How do you define a method?
+	you define a method by adding def with the name of the method then ending the method.
+What is the difference between a local variable and a method?
+	A local variable is a variable that is located inside a method for example
+	def cat(lazy, yellow)
+	end
+	in the example above cat is a method and lazy and yellow are local variables.
+How do you run a ruby program from the command line?
+	You go to the terminal and go to the file the is located then you type ruby then type the name of the file
+How do you run an RSpec file from the command line?
+	You go to the terminal and go to the file the is located then you type RSpec then type the name of the file
+What was confusing about this material? What made sense?
+	this material was not that consufing.
+=end
