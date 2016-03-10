7e231500fa575e773c8ab7b80d9b42a3c6e76757
Macbook Air <macbookair@Macbooks-Air.netgear.com>
2016-03-10 12:41:37 -0800

    add reflection

--- week-5/acct_groups.rb
+++ week-5/acct_groups.rb
@@ -1,6 +1,28 @@
+=begin
+pseudocode 
+input:all names of students
+output: split array with 5 names in each array
+steps:
+get all names into an array
+call array and apply the each_slice method and use the coder block to write how you want to print out ur arrays
+=end
+
+
 fiery_Skippers_2016 =['Aarthi Gurusami','Abid Ramay','Adam Zmudzinski','Alec Hendrickson','Alex Wen','Alicia Briceland'	,'Allison paul','Andrey Slonski','Anna Lansfjord','Ben Sanecki','Ben Flores','Buck Melton','Caitlin Hoffman','Carlos Gonzalez','Chand Nirankari','Ché Sanders','Chris Henderson','Chris Lamkin','Christyn Budzyna','Dan Park','David Ramirez','David Tao','David Walden','Bill Deng','Denny Jovic','Daniel Deutsch'	,'Dexter Moran','Diana Ozemebhoya Eromosele','Dominick Lombardo','Elan Kvitko','Elizabeth Alexander','Elizabeth Brown','Emmanuel Kaunitz','Ena Bek','Esther Leytush','Evan Druce','Frank Lam','Frank Lam','Gabriel Zurita','Jack Thatcher','Jason Milfred','John Colella','Jonathan Kaplan','Kelson Adams','Kristal Lam','Kunal','Kunal Patel','Leland Meiners','Liam Binell','Lisa Buch','Lisa Dannewitz','Lyudmila Arinich','Mohamed Monekata'	,'Parker Smathers','Patrick DeWitte','Renan Martins','RenanMartins','Riley Scheid'	,'Robin Soubry','Samantha Holmes','Scott Southard','Shaun R Sweet','Shin Wang','Sibel Ergener','Simon Thomas','Talal Talhouk','Ted Bogin','Traci Fong','Victoria Solorzano']
 
-
-
 fiery_Skippers_2016.each_slice(5) {|a| p a}
+
+=begin
+What was the most interesting and most difficult part of this challenge?
+	The most difficult part of this challenge was getting every student name inside an array.And the most interesting part was how I got to write the problem in two lines of code.
+Do you feel you are improving in your ability to write pseudocode and break the problem down?
+	I do feel like im improving in my pseudocode because it breaks down the challenge into steps that are easy to follow.
+Was your approach for automating this task a good solution? What could have made it even better?
+		My approach in automating I think was pretty good because I used a method to make it run the way I wanted
+What data structure did you decide to store the accountability groups in and why?
+	I Iterated a mehod to the the array and in the coder block had it print out the way I wanted it to.
+What did you learn in the process of refactoring your initial solution? Did you learn any new Ruby methods?
+	My initial solution was already refactored becaue i used a refactored method.
+=end
+
 
