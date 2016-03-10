9eea8008097c17277191a1ea18716ee8312d48ff
Macbook Air <macbookair@Macbooks-Air.netgear.com>
2016-03-09 21:47:17 -0800

    add all files

---
+++ week-5/nums-commas/my_solution.rb
@@ -0,0 +1,33 @@
+# Numbers to Commas Solo Challenge
+
+# I spent [] hours on this challenge.
+
+# Complete each step below according to the challenge directions and
+# include it in this file. Also make sure everything that isn't code
+# is commented in the file.
+
+# 0. Pseudocode
+
+# What is the input?
+# What is the output? (i.e. What should the code return?)
+# What are the steps needed to solve the problem?
+
+
+# 1. Initial Solution
+def separate_comma(num)
+	str = num.to_s
+	if str.length < 4
+		return str[0..3] + "," + str[4]
+	elsif str.length < 7
+		return
+
+end
+
+
+
+# 2. Refactored Solution
+
+
+
+
+# 3. Reflection
