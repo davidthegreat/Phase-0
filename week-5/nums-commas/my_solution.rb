baf0b2d643a7838f2dfc8a863fd486a1955f755b
Macbook Air <macbookair@Macbooks-Air.netgear.com>
2016-03-12 16:14:05 -0800

    add reflection

--- week-5/nums-commas/my_solution.rb
+++ week-5/nums-commas/my_solution.rb
@@ -8,18 +8,35 @@
 
 # 0. Pseudocode
 
-# What is the input?
-# What is the output? (i.e. What should the code return?)
+# What is the input? a number
+# What is the output? (i.e. What should the code return?) commas every 3 num
 # What are the steps needed to solve the problem?
+#turn the number into a string
+
 
 
 # 1. Initial Solution
 def separate_comma(num)
 	str = num.to_s
+	# return str if str.length < 4
+	# return str[0] + "," + str[1..3] if str.length < 5
+	# return str[0..1] + "," + str[2..4] if str.length < 6 
+	# return str[0..2] + "," + str[3..5] if str.length < 7
+	# return str[0] + "," + str[1..3] + "," + str[4..6] if str.length < 8
+	# return str[0..1] + "," + str[2..4] + "," + str[5..7] if str.length < 9
 	if str.length < 4
-		return str[0..3] + "," + str[4]
+		return str
+	elsif str.length < 5
+		return str[0] + "," + str[1..3]	
+	elsif str.length < 6
+		return str[0..1] + "," + str[2..4]
 	elsif str.length < 7
-		return
+		return str[0..2] + "," + str[3..5]
+	elsif str.length < 8
+		return str[0] + "," + str[1..3] + "," + str[4..6]
+	elsif str.length < 9
+		return str[0..1] + "," + str[2..4] + "," + str[5..7]
+	end
 
 end
 
