09f9431fd8ab21684a65c6b7032335a880db0958
Macbook Air <macbookair@Macbooks-Air.netgear.com>
2016-03-13 18:26:09 -0700

    add reflection

--- week-5/gps2_2.rb
+++ week-5/gps2_2.rb
@@ -2,19 +2,24 @@
 # input: string of items separated by spaces (example: "carrots apples cereal pizza")
 # steps: 
   # [fill in any steps here]
-  # set default quantity
+  # define a method to create a list
+  # set the arrays to split the string
+  # create a new hash
+  # set default quantity = 1
   # print the list to the console [can you use one of your other methods here?]
 # output: [what data type goes here, array or hash?]
 
 # Method to add an item to a list
 # input: item name and optional quantity
-# steps:
-# output:
+# steps:def method that takes two arguments
+# name of item
+# optional quantity
+# output: return updated list
 
 # Method to remove an item from the list
-# input:
-# steps:
-# output:
+# input: our list an item to remove
+# steps: delete key from the list
+# output:updated list
 
 # Method to update the quantity of an item
 # input:
@@ -22,6 +27,67 @@
 # output:
 
 # Method to print a list and make it look pretty
-# input:
+# input:our list
+# for each item 
 # steps:
 # output:
+
+def list(items)
+	split_list = items.split
+	list_hash = Hash.new(1)
+	split_list.each do |x|
+		list_hash[x] = 1
+	end
+	list_hash
+end
+
+def add_item(new_item,list, quantity =1)
+	list[new_item] = 1
+	list
+end
+
+def remove_item(list, item)
+ list.delete(item)
+ return list
+end
+
+def update_item(list, item, quantity=1)
+ 	list[item] = quantity
+ 	return list
+end
+
+def print(list)
+	list.each do |item, quantity|
+		puts "We have #{quantity} #{item}"
+	end
+end
+
+my_list = list("Lemonade apples cereal pizza")
+p add_item("bannana", my_list, 5)
+p remove_item(my_list, "apples")
+p update_item(my_list, "cereal", 4)
+p print(my_list)
+p update_item(my_list, "Lemonade", 2)
+p update_item(my_list, "tomato", 3)
+p update_item(my_list, "onion", 1)
+p update_item(my_list, "Ice Cream", 4)
+p remove_item(my_list, "Lemonade")
+p update_item(my_list, "Ice Cream", 1)
+p print(my_list)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- week-5/pad-array/my_solution.rb
+++ week-5/pad-array/my_solution.rb
@@ -19,17 +19,60 @@
 
 
 # 1. Initial Solution
+
 def pad!(array, min_size, value = nil) #destructive
-  # Your code here
+ if array.length - min_size < 0
+   difference = array.length - min_size
+   difference = difference.abs
+   for i in 0..difference - 1
+     array.push(value)
+   end
+ end
+ return array
 end
 
+
 def pad(array, min_size, value = nil) #non-destructive
-  # Your code here
+ new_array = array.clone
+ if new_array.length - min_size < 0
+   difference = new_array.length - min_size
+   difference = difference.abs
+   for i in 0..difference - 1
+     new_array.push(value)
+   end
+ end
+ return new_array
 end
 
 
 # 3. Refactored Solution
+def pad!(array, min_size, value = nil) #destructive
+ difference = min_size - array.length
+ if difference > 0 
+ 	difference.times do array.push(value) end
+ end
+ return array
+end
 
-
-
+def pad(array, min_size, value = nil) #non-destructive
+ new_array = array.clone
+ difference = min_size - new_array.length
+ if min_size - new_array.length > 0
+   difference.times do new_array.push(value) end
+ end
+ return new_array
+end
+ 
 # 4. Reflection
+# Were you successful in breaking the problem down into small steps?
+# 	We were successful breaking down the problem into small steps
+# Once you had written your pseudocode, were you able to easily translate it into code? What difficulties and successes did you have?
+# 	After we wrote our pseudocode it was pretty eassy to translate it to code, our pseudocode was straight foward to what we wrote in our code.
+# Was your initial solution successful at passing the tests? If so, why do you think that is? If not, what were the errors you encountered and what did you do to resolve them?
+# 	Our initial solution was successful passing the tests because our pseudocode helped us organize our code.
+# When you refactored, did you find any existing methods in Ruby to clean up your code?
+# 	When we refactoredwe found a couple of methods like .times method 
+# How readable is your solution? Did you and your pair choose descriptive variable names?
+# 	Our solution was very readable and out variable names are striaight foward.
+# What is the difference between destructive and non-destructive methods in your own words?
+# 	a destructive returns the new solution and non-destructive return the new solution and the old solution.
