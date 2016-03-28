4eefe9a04bc4478551d1eff1a0f6e7a0a76548ae
Macbook Air <macbookair@Macbooks-Air.netgear.com>
2016-03-27 22:08:00 -0700

    add reflection

---
+++ week-7/game.js
@@ -0,0 +1,56 @@
+// Design Basic Game Solo Challenge
+
+// This is a solo challenge
+
+// Your mission description:
+// Overall mission:
+// Goals:
+// Characters:
+// Objects:
+// Functions:
+
+// Pseudocode
+//
+//
+//
+//
+//
+
+// Initial Code
+function goodGuy(name) {
+	this.name = name;
+	this.health = 100;
+	this.looseHealth = function
+
+}
+
+function badGuy(name){
+	this.name = "badGuy"
+	this.health = 100;
+}
+
+
+
+
+var goodGuy = new Player('Tim');
+var enemy = new Player('Thug');
+// Refactored Code
+
+
+
+
+
+
+// Reflection
+//What was the most difficult part of this challenge?
+	the most difficult part about this challenge was thinking what to make a Game of.
+What did you learn about creating objects and functions that interact with one another?
+Did you learn about any new built-in methods you could use in your refactored solution? If so, what were they and how do they work?
+How can you access and manipulate properties of objects?
+//
+//
+//
+//
+//
+//
+//
