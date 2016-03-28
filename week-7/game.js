3e2a0e4a6903078c943abe94f084eb19041e7a50
Macbook Air <macbookair@Macbooks-Air.netgear.com>
2016-03-28 13:21:06 -0700

    add reflection

--- week-7/game.js
+++ week-7/game.js
@@ -2,12 +2,12 @@
 
 // This is a solo challenge
 
-// Your mission description:
-// Overall mission:
-// Goals:
-// Characters:
+// Your mission description: street fighter version game
+// Overall mission: goodguy fights a bad guy  
+// Goals: figth to the death
+// Characters: good guy vs bad guy
 // Objects:
-// Functions:
+// Functions: looseHealth going to be a fuunction that every time a player hits another looses heatlh
 
 // Pseudocode
 //
@@ -20,20 +20,21 @@
 function goodGuy(name) {
 	this.name = name;
 	this.health = 100;
-	this.looseHealth = function
+	this.looseHealth = function()
 
 }
 
 function badGuy(name){
 	this.name = "badGuy"
 	this.health = 100;
+	this.looseHealth = function()
 }
 
 
 
 
-var goodGuy = new Player('Tim');
-var enemy = new Player('Thug');
+var goodGuy = new Player('David');
+var badGuy = new Player('');
 // Refactored Code
 
 
@@ -43,10 +44,13 @@ var enemy = new Player('Thug');
 
 // Reflection
 //What was the most difficult part of this challenge?
-	the most difficult part about this challenge was thinking what to make a Game of.
-What did you learn about creating objects and functions that interact with one another?
-Did you learn about any new built-in methods you could use in your refactored solution? If so, what were they and how do they work?
-How can you access and manipulate properties of objects?
+// 	the most difficult part about this challenge was thinking what to make a Game of.
+// What did you learn about creating objects and functions that interact with one another?
+// 	 I used a function that both goodguy and badguy are going to use and that is looseHealth, scince both of them are going to use this function it works out perfect.
+// Did you learn about any new built-in methods you could use in your refactored solution? If so, what were they and how do they work?
+// 	I didnt finish my game so I didnt refactored my solution.
+// How can you access and manipulate properties of objects?
+// 	I used the constructor fuction which is like a class for ruby.
 //
 //
 //
