96e384265e686b34bd9d0327a4aebaa354ed1f50
Macbook Air <macbookair@Macbooks-Air.netgear.com>
2016-03-31 10:26:38 -0700

    add file

--- week-8/ruby/ruby_review.rb
+++ week-8/ruby/ruby_review.rb
@@ -13,27 +13,44 @@
 
 
 # Initial Solution
-def reverse_words(string)
-	if string.empty?
-		p string
-	else
-		words = string.split(' ')
-		words.each do |words| 
-			words.reverse!
-		end
-		p words.join(' ')
-	end
-end
+# def reverse_words(sentence)
+# 	if sentence.empty?
+# 		p sentence
+# 	else
+# 		words = sentence.split(' ')
+# 		words.each do |words| 
+# 			words.reverse!
+# 		end
+# 		p words.join(' ')
+# 	end
+# end
 
-reverse_words("Let's see if this works correctly!")
+# p reverse_words("Testing my work!")
 
 
 
 
 # Refactored Solution
+def reverse_words(sentence)
+	if sentence.empty?
+		p sentence
+	else
+		words = sentence.split(' ')
+		words.each do |words|  words.reverse!
+		end
+		p words.join(' ')
+	end
+end
+p reverse_words("Testing my work!")
 
 
 
 
 
 # Reflection
+# What concepts did you review or learn in this challenge?
+#	I reviewed the iterate over the array.
+# What is still confusing to you about Ruby?
+#	on this challenge noting was confusing, I felt that this challenge was straight foward review.
+# What are you going to study to get more prepared for Phase 1?
+#	I'm going to review the ruby methods page because I still havent memories all the methods
