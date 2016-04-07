337073ba1fb3be9b5ca1158413ee93c46fc834a7
Macbook Air <macbookair@Macbooks-Air.netgear.com>
2016-04-07 10:17:53 -0700

    add file

---
+++ week-9/dom-manipulation/home_page.js
@@ -0,0 +1,42 @@
+// DOM Manipulation Challenge
+
+
+// I worked on this challenge [by myself, with: ].
+
+
+// Add your JavaScript calls to this page:
+
+// Release 0:
+
+
+
+// Release 1:
+document.getElementById("release-0").className = "done";
+
+
+
+
+// Release 2:
+document.getElementById("release-1").style.display = "none";
+
+
+
+// Release 3:
+document.getElementsByTagName("h1")[0].innerHTML = "I completed release 2";
+
+
+
+
+// Release 4:
+document.getElementById("release-3").style.backgroundColor = "#955251";
+
+
+
+
+// Release 5:
+document.getElementById("release-3").style.backgroundColor = "#955251";
+
+// Release 7; Reflection
+// What did you learn about the DOM?
+
+//What are some useful methods to use to manipulate the DOM?
